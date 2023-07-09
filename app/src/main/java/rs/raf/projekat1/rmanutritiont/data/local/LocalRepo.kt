b260c9398866ab5e070cf4658b42bf5761e824c1
Nikola <djura97.nd@gmail.com>
2023-07-09 02:21:42 +0200

    Changed LocalRepo types from List to Set.

--- app/src/main/java/rs/raf/projekat1/rmanutritiont/data/local/LocalRepo.kt
+++ app/src/main/java/rs/raf/projekat1/rmanutritiont/data/local/LocalRepo.kt
@@ -3,11 +3,11 @@ package rs.raf.projekat1.rmanutritiont.data.local
 import rs.raf.projekat1.rmanutritiont.data.model.MealFromApi
 
 data class LocalRepo(
-    val meals: List<MealFromApi>?
+    val meals: Set<MealFromApi>?
 )
 
 data class FavoritesRepo(
-    val meals: List<MealFromApi>?
+    val meals: Set<MealFromApi>?
 )
 
 class LocalRepoImpl() {}
